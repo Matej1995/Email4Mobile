b8ce7ae2493da0bf577242d3d2daab3e3e43e416
Matej1995 <msandera95@gmail.com>
2018-07-25 23:35:21 +0200

    Navigace bottom fix and switch between fragments

---
+++ app/src/main/java/app/email4mobile/ui/activity/MainActivity.kt
@@ -0,0 +1,55 @@
+package app.email4mobile.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import app.email4mobile.R
+import app.email4mobile.ui.fragments.CalendarFragment
+import app.email4mobile.ui.fragments.EmailFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setFragment(EmailFragment())
+        setUpNav()
+
+    }
+
+    fun setUpNav() {
+        main_nav.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_email -> {
+                    val emailFragment = EmailFragment()
+                    setFragment(emailFragment)
+                    true
+                }
+                R.id.nav_calendar -> {
+                    val calendarFragment = CalendarFragment()
+                    setFragment(calendarFragment)
+                    true
+                }
+                R.id.nav_account -> {
+                    true
+                }
+                else -> {
+                    return@OnNavigationItemSelectedListener false
+                }
+            }
+        })
+    }
+
+    private fun setFragment(fragment: Fragment) {
+     val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.main_frame, fragment)
+        transaction.commit()
+    }
+}
+
+
+
